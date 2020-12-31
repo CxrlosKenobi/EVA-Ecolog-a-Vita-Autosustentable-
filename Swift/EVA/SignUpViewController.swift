472b6202198a07a06264426c9745b705366f39b6
CxrlosKenobi <yolitopinto@gmail.com>
2020-12-31 16:00:27 -0300

    Update: XCode files uploaded!

---
+++ Swift/EVA/SignUpViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SignUpViewController.swift
+//  EVA
+//
+//  Created by Kenobi on 28-12-20.
+//  Copyright © 2020 KenobiLab. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+    @IBAction func signUpTapped(_ sender: Any) {
+    }
+
+
+    @IBAction func alreadyHaveAnAccountTapped(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "login")
+        vc.modalPresentationStyle = .overFullScreen
+        present(vc, animated: true)
+    }
+
+}
